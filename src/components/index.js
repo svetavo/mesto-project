0029b7be7314a7e0c17880176280c83378d39442
Sveta Vostrikova <vo.cherepash@yandex.ru>
2022-09-02 12:27:00 +0200

    api

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
 // импорт
 import '../styles/index.css';
-import { enableValidation, setPlaceSubmitButtonState } from "./validate";
-import { formProfile, formPlace, editButton, popUpEdit, addButton, popUpPlace, placeImgInput, placeNameInput, placesItems, profileName, profileDescription, profileNameInput, profileDescriptionInput, placesCards, validitySettings, popups } from './utils';
+import { enableValidation } from "./validate";
+import { avatarUrl, avatarButton, formProfile, popUpAvatar, formPlace, editButton, popUpEdit, addButton, popUpPlace, placeImgInput, placeNameInput, placesItems, profileName, profileDescription, profileNameInput, profileDescriptionInput, popups, formAvatar, avatar, renderLoading, placeSubmit } from './utils';
 import { createPlace } from "./card";
 import { openPopup, closePopup} from './modal';
+import { renderCards, renderInfo, newInfo, createCard, serverMe } from './api';
 
 // изображения
 // const closeImage = new URL('../images/CloseIcon.svg', import.meta.url);
@@ -13,6 +14,8 @@ const pencilSymbol = new URL('../images/symbols/pencil.png', import.meta.url);
 const plusSymbol = new URL('../images/symbols/plus.png', import.meta.url);
 const trashSymbol = new URL('../images/symbols/trash.svg', import.meta.url);
 const logoImage = new URL('../images/logo_header.svg', import.meta.url);
+
+let userId = serverMe;
 
 // СЛУШАТЕЛИ
 
@@ -30,34 +33,82 @@ popups.forEach((popup) => {
 
 // редактировать профиль
 editButton.addEventListener('click', () => {
-  openPopup(popUpEdit);
   profileNameInput.value = profileName.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
+  openPopup(popUpEdit);
 });
 
 formProfile.addEventListener('submit', (evt) => {
   evt.preventDefault();
+  renderLoading(true);
   profileName.textContent = profileNameInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
+  newInfo();
   closePopup(popUpEdit);
 });
 
+// редактировать аватар
+avatarButton.addEventListener('click', () => {
+  avatar.value = avatarUrl.src;
+  openPopup(popUpAvatar);
+})
+
+formAvatar.addEventListener('submit', () => {
+  evt.preventDefault();
+  renderLoading(true);
+  avatarUrl.src = avatar.value;
+  avatarUpdate();
+  closePopup(popUpAvatar);
+})
+
 // добавить место
 addButton.addEventListener('click', () => {
+  formPlace.reset();
   openPopup(popUpPlace);
 });
 
 formPlace.addEventListener('submit',(evt) => {
   evt.preventDefault();
-  placesItems.prepend(createPlace(placeNameInput.value, placeImgInput.value));
-  closePopup(popUpPlace);
-  formPlace.reset();
-  setPlaceSubmitButtonState(false);
+  renderLoading(true);
+  createCard()
+  // почему то андейфайд?? пост уходит на сервер, а дальше все...
+  .then((result) => {
+    const myCard = createPlace(result.name, result.link, result.likes, result._id, result.owner)
+    placesItems.prepend(myCard);
+    formPlace.reset();
+    placeSubmit.classList.add('form__button_disabled');
+    placeSubmit.disabled = true;
+    closePopup(popUpPlace);
+  })
+});
+
+// загрузка инфо пользователя
+renderInfo()
+.then((data) => {
+  profileName.textContent = data.name;
+  profileDescription.textContent = data.about;
+  avatar.src = data.avatar;
+})
+.catch((err) => {
+  console.log(err);
 });
 
 // загрузка массива карточек
-placesCards.forEach((item) => {
-  placesItems.append(createPlace(item.name,item.link));
+renderCards()
+.then(data =>
+  data.forEach((item) => {
+      placesItems.append(createPlace(item.name, item.link, item.likes, item._id, item.owner ));
+  }))
+.catch((err) => {
+  console.log(err);
+  });
+
+enableValidation({
+formSelector: '.form',
+inputSelector: '.form__input',
+submitButtonSelector: '.form__button',
+inactiveButtonClass: 'form__button_disabled',
+inputErrorClass: 'form__input_type_error',
+errorClass: 'form__input-error_active'
 });
 
-enableValidation(validitySettings);
--- src/components/utils.js
+++ src/components/utils.js
@@ -2,8 +2,10 @@
 // *** селекторы ***
 export const editButton = document.querySelector('.profile__edit-button');
 export const popUpEdit = document.querySelector('.popup-edit');
+export const popUpDelete = document.querySelector('.popup-delete');
 export const addButton = document.querySelector('.profile__add-button');
 export const popUpPlace = document.querySelector('.popup-add-place');
+export const popUpAvatar = document.querySelector('.popup-avatar');
 export const placeSubmit = document.querySelector('.form__button_place');
 export const placesItems = document.querySelector('.places__items');
 export const popups = document.querySelectorAll('.popup');
@@ -12,11 +14,17 @@ export const profileName = document.querySelector('.profile__name');
 export const profileDescription = document.querySelector('.profile__description');
 export const profileNameInput = document.querySelector('#user_name');
 export const profileDescriptionInput = document.querySelector('#user_description');
+export const avatar = document.querySelector('.profile__pic');
+export const avatarButton = document.querySelector('.profile__avatar-button');
+export const cards = document.querySelector('.places');
+export const formButtons = document.querySelectorAll('.form__button');
 
 // переменные для валидации
 export const formProfile = document.forms.profile_data;
 export const name = formProfile.elements.name;
 export const description = formProfile.elements.description;
+export const formAvatar = document.forms.avatar;
+export const avatarUrl = formAvatar.elements.avatarUrl;
 export const validitySettings = {
   formSelector: '.form',
   inputSelector: '.form__input',
@@ -26,23 +34,6 @@ export const validitySettings = {
   errorClass: 'form__input-error_active'
 };
 
-//изображения
-const barcelona = new URL('../images/places/barcelona.jpeg', import.meta.url);
-const beijing = new URL('../images/places/beijing.jpeg', import.meta.url);
-const london = new URL('../images/places/london.jpeg', import.meta.url);
-const newyork = new URL('../images/places/new_york.jpeg', import.meta.url);
-const sydney = new URL('../images/places/sydney.jpeg', import.meta.url);
-const tallin = new URL('../images/places/tallinn.jpeg', import.meta.url)
-
-export const placesCards = [
-  { name: 'Барселона', link: barcelona },
-  { name: 'Пекин', link: beijing },
-  { name: 'Лондон', link: london },
-  { name: 'Нью-Йорк', link: newyork },
-  { name: 'Сидней', link: sydney },
-  { name: 'Таллин', link: tallin },
-];
-
 // переменные карточка
 export const placeTemplate = document.querySelector('#place-item').content;
 export const placeImgInput = document.querySelector('#placeImg');
@@ -50,3 +41,15 @@ export const placeNameInput = document.querySelector('#placeName');
 export const popUpImage = document.querySelector('.popup__image');
 export const imageContainer = document.querySelector('.popup__image-content');
 export const popupImageTitle = document.querySelector('.popup__image-title');
+
+// изменение кнопки во время загрузки
+export function renderLoading(isLoading) {
+  if(isLoading) {
+    formButtons.forEach((button) => {
+      button.textContent = 'Сохранение...';
+    })
+  }
+}
+
+
+
